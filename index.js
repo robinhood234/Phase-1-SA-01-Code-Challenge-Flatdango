9dba47ab402822e33435d58143ff58b350f73fa6
robin nganga smith <saintofcookies.2021@gmail.com>
2023-10-16 16:16:57 +0300

    function that will change content by 1 looks at id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
- fetch('http://localhost:3000/films/2')
+ fetch('http://localhost:3000/films/1')
   .then(response => response.json())
   .then(data => {
+
     const posterUrl = data.poster;
     const title = data.title;
     const runtime = data.runtime;
@@ -8,7 +9,9 @@
     const capacity = data.capacity;
     const ticketsSold = data.tickets_sold;
     const availableTickets = capacity - ticketsSold;
+    // document.getElementById('next').addEventListener((e)=>{
 
+    // })
     document.getElementById('poster').src = posterUrl;
     document.getElementById('title').textContent = title;
     document.getElementById('runtime').textContent = `${runtime} minutes`;
@@ -18,3 +21,27 @@
   .catch(error => {
     console.error('Error:', error);
   });
+
+  let currentMovieId = 1;
+
+function changeContent() {
+  currentMovieId++;
+  
+  fetch(`http://localhost:3000/films/${currentMovieId}`)
+    .then(response => response.json())
+    .then(data => {
+      const posterUrl = data.poster;
+      const title = data.title;
+      const runtime = data.runtime;
+      const showtime = data.showtime;
+      const capacity = data.capacity;
+      const ticketsSold = data.tickets_sold;
+      const availableTickets = capacity - ticketsSold;
+
+      document.getElementById('poster').src = posterUrl;
+      document.getElementById('title').textContent = title;
+      document.getElementById('runtime').textContent = `${runtime} minutes`;
+      document.getElementById('showtime').textContent = showtime;
+      document.getElementById('available-tickets').textContent = availableTickets;
+    });
+}
